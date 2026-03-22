d5604b27beb59b59ec38c1f2c1d3a858b14e7726
DanMadar27 <danmadar27@gmail.com>
2026-03-22 08:55:09 +0200

    Add backend to aws terraform

--- terraform/aws/terraform.tf
+++ terraform/aws/terraform.tf
@@ -1,4 +1,12 @@
 terraform {
+  # Configure remote state in S3
+  #   backend "s3" {
+  #   bucket       = "bucket-name"
+  #   key          = "path/terraform.tfstate"
+  #   region       = "region-name"
+  #   use_lockfile = true # Lock file is used to prevent race conditions when multiple terraform processes try to access the same state file.
+  # }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
