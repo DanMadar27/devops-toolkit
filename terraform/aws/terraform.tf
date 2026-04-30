d85554c29d12eeee1ae5b0b988afa8564e2633f6
DanMadar27 <danmadar27@gmail.com>
2026-04-30 10:31:51 +0300

    AAdd lifecycle rule comment about s3 terraform backend

--- terraform/aws/terraform.tf
+++ terraform/aws/terraform.tf
@@ -1,5 +1,17 @@
 terraform {
-  # Configure remote state in S3
+  # Configure remote state in S3 (with enabled versioning)
+  /*
+  You can create lifecycle rule:
+    - name: delete_old_versions
+    - Scope: Entire bucket
+    - Noncurrent versions actions:
+      - day 30: 
+          - 2 newest noncurrent versions are retained
+          - All other noncurrent versions are permanently deleted
+    - Delete expired object delete markers or incomplete multipart uploads
+        - Delete expired object delete markers
+        - Incomplete multipart uploads: Delete after 30 days
+  */
   #   backend "s3" {
   #   bucket       = "bucket-name"
   #   key          = "path/terraform.tfstate"
